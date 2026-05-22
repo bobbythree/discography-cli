f39485e995c94296c0eca12e776f3e5bf756edf3
bobbythree <bobbythr33@gmail.com>
2026-05-21 22:21:16 -0500

    fix package declaratoins and add http client

--- discogsapi/client.go
+++ discogsapi/client.go
@@ -1,8 +1,15 @@
-package search
+package discogsapi
 
 import "net/http"
 
 type Client struct {
-	httpClient http.Client
+	httpClient *http.Client
 	baseURL    string
+}
+
+func NewClient() *Client {
+	return &Client{
+		httpClient: &http.Client{},
+		baseURL:    "https://api.discogs.com",
+	}
 }
